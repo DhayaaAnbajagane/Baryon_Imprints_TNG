0553fbc3d365f81fab0e0c66b149ed5139d4c1de
Dhayaa Anbajagane <dhayaaanbajagane@gmail.com>
2021-09-06 17:16:20 -0500

    Redo directory assignment

--- Interpolator/__init__.py
+++ Interpolator/__init__.py
@@ -1,1 +1,1 @@
-from .Interpolator import Scaling_Relation, Correlation, Covariance, avail_sims, avail_properties, avail_params
+from .__Interpolator import Scaling_Relation, Correlation, Covariance, avail_sims, avail_properties, avail_params
